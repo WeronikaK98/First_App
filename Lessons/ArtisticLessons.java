17ae944db40f3c59b6c5e4655b962800fc624b39
WeronikaK98 <w.kulczycka98@gmail.com>
2023-05-15 18:31:51 +0200

    Commit 15.05.2023.

--- Lessons/ArtisticLessons.java
+++ Lessons/ArtisticLessons.java
@@ -4,8 +4,42 @@ public class ArtisticLessons extends Lesson {
     public ArtisticLessons(String lessonName, String day, String time, int price, int limitChildrenInGroup) {
         super(lessonName, day, time, price, limitChildrenInGroup);
     }
-    ArtisticLessons rythmic = new ArtisticLessons("Rytmika", "Środa", "17:00", 40, 10);
-    ArtisticLessons painting = new ArtisticLessons("Malarstwo", "Wtorek", "16:00", 30, 20);
-    ArtisticLessons piano = new ArtisticLessons("Fortepian", "Czwartek", "16:00", 60, 1);
+
+    org.example.Lessons.ArtisticLessons rythmic = new org.example.Lessons.ArtisticLessons("Rytmika", "Środa", "17:00", 40, 10);
+    org.example.Lessons.ArtisticLessons painting = new org.example.Lessons.ArtisticLessons("Malarstwo", "Wtorek", "16:00", 30, 20);
+    org.example.Lessons.ArtisticLessons piano = new org.example.Lessons.ArtisticLessons("Fortepian", "Czwartek", "16:00", 60, 1);
+
+    public ArtisticLessons(String lessonName, String day, String time, int price, int limitChildrenInGroup, org.example.Lessons.ArtisticLessons rythmic, org.example.Lessons.ArtisticLessons painting, org.example.Lessons.ArtisticLessons piano) {
+        super(lessonName, day, time, price, limitChildrenInGroup);
+        this.rythmic = rythmic;
+        this.painting = painting;
+        this.piano = piano;
+    }
+
+    public org.example.Lessons.ArtisticLessons getRythmic() {
+        return rythmic;
+    }
+
+    public void setRythmic(org.example.Lessons.ArtisticLessons rythmic) {
+        this.rythmic = rythmic;
+    }
+
+    public org.example.Lessons.ArtisticLessons getPainting() {
+        return painting;
+    }
+
+    public void setPainting(org.example.Lessons.ArtisticLessons painting) {
+        this.painting = painting;
+    }
+
+    public org.example.Lessons.ArtisticLessons getPiano() {
+        return piano;
+    }
+
+    public void setPiano(org.example.Lessons.ArtisticLessons piano) {
+        this.piano = piano;
+    }
+
+
 }
 
--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-//import package.Child.class
+import java.util.*;
+
+import org.example.People.Child;
+//import package.Child.class;
 //import java.lang.runtime.SwitchBootstraps;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -11,6 +12,9 @@ import org.example.People.Parent;
 import org.example.Lessons.Lesson;
 import org.example.Lessons.SportActivity;
 import org.example.Lessons.ArtisticLessons;
+import java.util.logging.*;
+import java.io.IOException;
+import java.lang.Exception;
 
 public class Main {
     public static void main(String[] args)
@@ -22,10 +26,50 @@ public class Main {
 //        childs.add(fourthChild);
 //        childs.add(fifthChild);
 
-        int option = 2;
-        int option2 = 1;
+//        int option = 2;
+//        int option2 = 1;
+        String login = "Weronika";
+        String password = "1477";
+        Logger logger = Logger.getLogger("org.example");
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Profil rodzica");
+        System.out.println("Zaloguj się");
+        System.out.println("Login: " );
+
+        Scanner console1 = new Scanner(System.in);
+        String line1 = console1.nextLine();
+
+        if (!line1.equals("Weronika"))
+        {
+            System.out.println("Nie znaleziono użytkownika");
+            System.out.println("Upewnij się, że login jest poprawny");
+            logger.log(Level.INFO, "Nieudana próba logowania");
+            System.out.println("Login: " );
+            line1 = console1.nextLine();
+        } else {logger.log(Level.INFO, "Logowanie użytkownika");}
+
+        System.out.println("Hasło: ");
+//        Scanner console = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        if (!line.equals("1477")) {
+            System.out.println("Nieprawidłowe hasło");
+            System.out.println("Spróbuj ponownie");
+            logger.log(Level.INFO, "Nieudana próba logowania");
+            System.out.println("Hasło: ");
+            line = scanner.nextLine();
+        } else {
+            System.out.println("Dzień dobry");
+            logger.log(Level.INFO, "Logowanie użytkownika");
+        }
+
+        System.out.println("1. Dane opiekuna");
+        System.out.println("2. Moje dzieci");
+        System.out.println("3. Oferta");
+
+//        Scanner scanner = new Scanner(System.in);
+        int option = Integer.parseInt(scanner.nextLine());
 
         switch (option)
         {
@@ -35,22 +79,55 @@ public class Main {
 
             case 2:
                 System.out.println("Moje dzieci");
+                System.out.println("1. Maks");
+                System.out.println("2. Zosia");
+                System.out.println("3. Utwórz nowy profil");
+
+//                Scanner scanner2 = new Scanner(System.in);
+                int option2 = Integer.parseInt(scanner.nextLine());
                 switch (option2) {
                     case 1:
                         System.out.println("Maks");
-//                        Child firstChild = new Child("Maks", "21233000111", 1, "Gdańsk");
-//                        System.out.println(firstChild.name);
-//                        System.out.println(firstChild.pesel);
-//                        System.out.println(firstChild.age);
-//                        System.out.println(firstChild.address);
+                        try {
+                            Child firstChild = null;
+                            System.out.println(firstChild.getName());
+                            System.out.println(firstChild.getPesel());
+                            System.out.println(firstChild.getAge());
+                            System.out.println(firstChild.getAddress());
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                            logger.log(Level.WARNING, "firstChild is null");
+                            throw e;
+                        }
                         break;
                     case 2:
                         System.out.println("Zosia");
-                        //wyświetlić secondChild z Child
+                        try {
+                            Child secondChild = null;
+                            System.out.println(secondChild.getName());
+                            System.out.println(secondChild.getPesel());
+                            System.out.println(secondChild.getAge());
+                            System.out.println(secondChild.getAddress());
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                            logger.log(Level.WARNING, "secondChild is null");
+                            throw e;
+                        }
                         break;
                     case 3:
                         System.out.println("Utwórz nowy profil");
-                        //Child nextChild = new Child();
+                        try {
+                            Child nextChild = null;
+                            System.out.println("Imię: ");
+                            System.out.println("Pesel: ");
+                            System.out.println("Wiek: ");
+                            System.out.println("Adres: ");
+                            logger.log(Level.INFO, "Utworzono nowy profil");
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                            logger.log(Level.WARNING, "nextChild is null");
+                            throw e;
+                        }
                         break;
                 }
 
@@ -59,5 +136,11 @@ public class Main {
                 //wyświetlić obiekty z SportActivity i ArtisticLessons
 
         }
+    }
+}
+
+class CustomException extends Exception {
+    public CustomException (String message) {
+        super(message);
     }
 }
